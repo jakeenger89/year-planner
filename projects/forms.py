93d46d4b2e79d0d277877530de6d4d4dae99bbf3
Jake <jakeenger@yahoo.com>
2023-09-06 11:13:23 -0700

    feature 14

---
+++ projects/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from projects.models import Project
+
+
+class CreateProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = (
+            "name",
+            "description",
+            "owner",
+        )
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
+from projects.forms import CreateProjectForm
 
 # Create your views here.
 
@@ -19,3 +20,20 @@ def show_project(request, id):
         "show_project": show_project,
     }
     return render(request, "detail.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = CreateProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list_projects")
+    else:
+        form = CreateProjectForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "create.html", context)
